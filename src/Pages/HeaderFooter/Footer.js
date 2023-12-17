b152b59da6e78e3abf09fb7faaeeba04730a8dfd
Francis <k1k1dipsen@yahoo.fr>
2023-12-17 17:53:50 +0100

    test

--- src/Pages/HeaderFooter/Footer.js
+++ src/Pages/HeaderFooter/Footer.js
@@ -6,24 +6,32 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <footer className="colorFooter">
-      <div className={`df fc jcc aic fsize2 ${styles.footerLogo}`}>
-        <p>LILLE</p>
-        <p>LATINA</p>
-        <p className="ffamily3">Dance</p>
+      <div className={styles.icons}>
+        <a href="https://www.facebook.com/">
+          <img className="ml20" src={fb} alt="logo de facebook" />
+        </a>
+        <div className={`df fc jcc aic fsize2 ${styles.footerLogo}`}>
+          <p>LILLE</p>
+          <p>LATINA</p>
+          <p className="ffamily3">Dance</p>
+        </div>
+        <a href="https://www.instagram.com/">
+          <img src={insta} alt="logo de instagram" />
+        </a>
       </div>
       <div className={`df jcc fsize08 ${styles.footerend}`}>
         <p>Copyright©2023 |Tous droits réservés -</p>
         <span>
           <Link to="/mentions">Mentions légales</Link>
         </span>
-        <div className={styles.icons}>
-          <a href="">
+        {/* <div className={styles.icons}>
+          <a href="https://www.facebook.com/">
             <img className="ml20" src={fb} alt="logo de facebook" />
           </a>
-          <a href="">
+          <a href="https://www.instagram.com/">
             <img src={insta} alt="logo de instagram" />
           </a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
